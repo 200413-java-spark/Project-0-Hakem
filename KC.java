a2ae7eb4826e98206751c523bfd16d1b7e15b84e
Hakem the Dream <hakemawan@gmail.com>
2020-04-14 10:40:57 -0700

    Update

--- KC.java
+++ KC.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 class KC {
     public static void main(String[] args) {
-
+        
+        Scanner input = new Scanner(System.in);
         System.out.println("Choose Starting Kingdom: 1. Elves 2. Humans 3. Dwarves 4. Goblins");
+        String kingdom = input.nextLine();
+        if (kingdom == "1") {
+        Elf king1 = new Elf();
+        }else if (kingdom == "2") {
+            Human king2 = new Human();
+        }else if (kingdom == "3") {
+            Dwarf king3 = new Dwarf();
+        }else if (kingdom == "4") {
+            Goblin king4 = new Goblin();
+        }
 
-        //Elf king1 = new Elf();
+        
 
+        
     }
 }
